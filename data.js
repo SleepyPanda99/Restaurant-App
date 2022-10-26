4ad3d054c6a4a09fdf38d38e9e7f1300914aa545
SleepyPanda99 <113494724+SleepyPanda99@users.noreply.github.com>
2022-10-26 16:14:04 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "The Classic",
+        ingredients: ["Comes with beef" , " cheese" , " tomato" , " lettuce"],
+        price: "$3.95",
+        menuIcon: "images/burgericon2.png",
+         id: '0',
+        
+    },
+    {
+        name: "Don't You For Cheddar 'Bout Me",
+        ingredients: ["Comes with four kinds of cheddar"],
+        price: "$5.95",
+        menuIcon: "images/burgericon2.png",
+        id: '1'
+    },
+        {
+        name: "Sweet Home Avocado",
+        ingredients: ["Comes with Avocado"],
+        price: "$5.95",
+        menuIcon: "images/burgericon2.png",
+        id: '2'
+    },
+     {
+        name: "Eggers Can't Be Cheesers",
+        ingredients: ["Comes with an over-easy egg"],
+        price: "$5.95",
+       menuIcon: "images/burgericon2.png",
+        id: '3'
+    },
+     {
+        name: "Gordon Hamsy",
+        ingredients: ["Comes with ham"],
+        price: "$5.95",
+        menuIcon: "images/burgericon2.png",
+        id: '4'
+    },
+]
