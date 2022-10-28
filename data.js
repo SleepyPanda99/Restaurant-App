440594e47b67ba63519bfdb5335bb407f7b9422e
SleepyPanda99 <113494724+SleepyPanda99@users.noreply.github.com>
2022-10-28 14:27:36 -0400

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export default [
         name: "The Classic",
         ingredients: ["Comes with beef" , " cheese" , " tomato" , " lettuce"],
         price: "$3.95",
-        menuIcon: "images/burgericon2.png",
+        menuIcon: "url(https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg)",
          id: '0',
         
     },
@@ -11,28 +11,28 @@ export default [
         name: "Don't You For Cheddar 'Bout Me",
         ingredients: ["Comes with four kinds of cheddar"],
         price: "$5.95",
-        menuIcon: "images/burgericon2.png",
+        menuIcon: "url(https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg)",
         id: '1'
     },
         {
         name: "Sweet Home Avocado",
         ingredients: ["Comes with Avocado"],
         price: "$5.95",
-        menuIcon: "images/burgericon2.png",
+        menuIcon: "url(https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg)",
         id: '2'
     },
      {
         name: "Eggers Can't Be Cheesers",
         ingredients: ["Comes with an over-easy egg"],
         price: "$5.95",
-       menuIcon: "images/burgericon2.png",
+       menuIcon: "url(https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg)",
         id: '3'
     },
      {
         name: "Gordon Hamsy",
         ingredients: ["Comes with ham"],
         price: "$5.95",
-        menuIcon: "images/burgericon2.png",
+        menuIcon: "url(https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg)",
         id: '4'
     },
 ]
