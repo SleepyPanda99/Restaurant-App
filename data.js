e859b78c37fc191fbeb73a76f9d1df7d24ad3c9a
SleepyPanda99 <113494724+SleepyPanda99@users.noreply.github.com>
2022-10-28 14:46:37 -0400

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export default [
     {
         name: "The Classic",
         ingredients: ["Comes with beef" , " cheese" , " tomato" , " lettuce"],
-        price: 4 ,
+        price: '4' ,
         menuIcon: "https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg",
          id: '0',
         
@@ -11,28 +11,28 @@ export default [
     {
         name: "Don't You For Cheddar 'Bout Me",
         ingredients: ["Comes with four kinds of cheddar"],
-        price: 6 ,
+        price: '6' ,
         menuIcon: "https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg",
         id: '1'
     },
         {
         name: "Sweet Home Avocado",
         ingredients: ["Comes with Avocado"],
-        price: 6 ,
+        price: '6' ,
         menuIcon: "https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg",
         id: '2'
     },
      {
         name: "Eggers Can't Be Cheesers",
         ingredients: ["Comes with an over-easy egg"],
-        price: 6 ,
+        price: '6' ,
        menuIcon: "https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg",
         id: '3'
     },
      {
         name: "Gordon Hamsy",
         ingredients: ["Comes with ham"],
-        price: 6 ,
+        price: '6' ,
         menuIcon: "https://admin.itsnicethat.com/images/eGg344x1TC9G5vIgbLJVjzsoWmA=/105409/width-1440/591369447fa44caff50006a1.jpg",
         id: '4'
     },
